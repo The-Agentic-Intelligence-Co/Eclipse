bae6402d64dfa63d49f1101a61173dd5a612a9c2
Luis Chapa <103275464+ficiency@users.noreply.github.com>
2025-10-07 20:32:09 -0600

    clean: removed unused old schemas folder

--- src/multiagents/schemas/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Índice de esquemas JSON para todos los agentes
- */
-
-// Currently unused. Will be used in the future when we have more LLM providers, 
-// since groq doesn't support json schemas combined with tools yet.
-
-export * from './plannerSchemas';
-export * from './validatorSchemas';
